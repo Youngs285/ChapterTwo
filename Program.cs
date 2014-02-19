bc34d78c3d5e646bcececaecdfeed1a277f348c7
Youngs285 <nitronite500@gmail.com>
2014-02-19 10:11:51 -0500

    Initial

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Length = 15, Width = 25, area;
+            
+
+            area = Width * Length;
+
+            Console.WriteLine("The floor space is {0} square feet", area);
+        }
+    }
+}
