678952f5f161a2e35a26d3a93555fe8ff3a2de74
Youngs285 <nitronite500@gmail.com>
2014-02-20 10:09:14 -0500

    new

--- Carpet/Program.cs
+++ Carpet/Program.cs
@@ -9,6 +9,8 @@ namespace Carpet
     {
         static void Main(string[] args)
         {
+            double Length = 15, Width = 25, Cost = 5.67;
+            Console.Write("The width of the room is {0} Square feet", Width);
         }
     }
 }
